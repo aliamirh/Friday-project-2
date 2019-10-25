d804c989cd4b3c5a6ab1563c8bf23981a725b796
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-10-25 16:37:34 -0700

    add if statements and working language quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
   $("#test").submit(function(event){
     event.preventDefault();
-    var favoriteDay = parseInt($("select#favoriteDay").val());
-    var sports = parseInt($("select#sports").val());
-    var games = parseInt($("select#games").val());
-    var car = parseInt($("select#car").val());
-    var pet = parseInt($("select#pet").val());
+    var favoriteDayNum = parseInt($("select#favoriteDay").val());
+    var sportsNum = parseInt($("select#sports").val());
+    var gamesNum = parseInt($("select#games").val());
+    var carNum = parseInt($("select#car").val());
+    var petNum = parseInt($("select#pet").val());
+    console.log(petNum);
+    var addNum = favoriteDayNum + sportsNum + gamesNum + carNum + petNum;
+
+    if(addNum <=10){
+      $(".csharp").show();
+    }else if ( addNum === 20 ){
+      $(".python").show();
+    }else{
+      $(".javascript").show();
+    }
+  });
+});
